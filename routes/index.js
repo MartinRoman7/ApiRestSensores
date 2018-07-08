108c6d312a4b0079e22f4bc759b8a68ebfd462b2
Martin Roman <martin@localhost.localdomain>
2018-07-08 01:22:35 -0500

    Methods

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/api/puppies', db.getAllPuppies);
+router.get('/api/puppies/:id', db.getSinglePuppy);
+router.post('/api/puppies', db.createPuppy);
+router.put('/api/puppies/:id', db.updatePuppy);
+router.delete('/api/puppies/:id', db.removePuppy);
 
 module.exports = router;
